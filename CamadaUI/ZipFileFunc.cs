a56880e028d3e3dafab92fc218569614f2c630c3
Daniel Ramos de Oliveira <danielroliveira@gmail.com>
2019-11-26 17:20:42 -0300

    zip functions creating - part1

---
+++ CamadaUI/ZipFileFunc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+
+namespace CamadaUI
+{
+	public class ZipFileFunc
+	{
+		public bool CreateZipFromDirectory()
+		{
+			string zipPath = @"c:\users\exampleuser\start.zip";
+			string extractPath = @"c:\users\exampleuser\extract";
+			string newFile = @"c:\users\exampleuser\NewFile.txt";
+
+			using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+			{
+				archive.CreateEntryFromFile(newFile, "NewEntry.txt");
+				archive.ExtractToDirectory(extractPath);
+			}
+
+			return true;
+
+		}
+	}
+}
